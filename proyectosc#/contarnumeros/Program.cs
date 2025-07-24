ae4e49178221205c5a72bda051c4d55c0991e928
mercally1 <jmercally@gmail.com>
2025-07-24 12:26:30 -0600

    work out new,numbers count

---
+++ proyectosc#/contarnumeros/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("<<<<<Una forma>>>>>");
+
+        for (int i = 0; i <= 100; i++)
+        {
+            Console.WriteLine("Numero " + i);
+        }
+
+        Console.WriteLine("\n<<<<<forma dos>>>>>");
+
+        int number = 1;
+        while (number <= 100)
+        {
+            Console.WriteLine("El nunero " + number + "\n");
+            number++;
+        }
+    }
+}
