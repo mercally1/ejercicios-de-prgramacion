a278646ad8699cd5152f16bd0d481ba1345eaf0d
mercally1 <jmercally@gmail.com>
2025-07-24 10:57:06 -0600

    first commit

---
+++ proyectosc#/binario/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("BIENVENIDO AL PROGRAMA DECINARIO");
+
+        int numerodecimal = int.Parse(Console.ReadLine());
+        string binario = Convert.ToString(numerodecimal, 2);
+
+            Console.WriteLine($" El numero decimal de {numerodecimal}.\n Numero binario {binario}.");
+    }
+}
---
+++ proyectosc#/numeroprimo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        //Console.WriteLine("Ingrese un numero");
+        //int numero  = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= 100; i ++)
+        {
+            
+            
+        string resultado = (i / 1 == 0) && (i/ i == 0 ) ? "primo" : "No primo";
+
+        Console.WriteLine($"Es numero {i} es un numero {resultado}\n");
+            
+        }
+    }
+}
