a675acf02de93f1f0d6f4cf8acfa98f3de32f527
mercally1 <jmercally@gmail.com>
2025-07-24 13:34:57 -0600

    new way count number

--- proyectosc#/contarnumeros/Program.cs
+++ proyectosc#/contarnumeros/Program.cs
@@ -6,6 +6,7 @@ class Program
 {
     public static void Main(string[] args)
     {
+        //forma uno 
         Console.WriteLine("<<<<<Una forma>>>>>");
 
         for (int i = 0; i <= 100; i++)
@@ -13,6 +14,7 @@ class Program
             Console.WriteLine("Numero " + i);
         }
 
+        //forma dos
         Console.WriteLine("\n<<<<<forma dos>>>>>");
 
         int number = 1;
@@ -21,5 +23,14 @@ class Program
             Console.WriteLine("El nunero " + number + "\n");
             number++;
         }
+
+        //forma tres
+        Console.WriteLine("<<<<< tercera forma >>>>>");
+        int contador = 1; 
+        do
+        {
+            Console.WriteLine("El numero" + contador + "\n");
+            contador++;
+        } while (contador >= 100);
     }
 }
