e55eb5aa32929d566d3cde7178d89a51e34c19fa
mercally1 <jmercally@gmail.com>
2025-07-24 15:58:52 -0600

    new proyecto area triangulo

---
+++ proyectosc#/areatriangulo/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Calcula el area de un triangulo");
+
+        Console.WriteLine("Introduce la base de triangulo");
+        decimal areabase = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce la altura del triangulo");
+        decimal altura = decimal.Parse(Console.ReadLine());
+
+        decimal resultado = Math.Round((areabase * altura / 2), 2);
+
+        Console.WriteLine($"la area del triangulo es {resultado}");
+    }
+}
