fb4a616d600a9adf2527964d248c925e72267973
mercally1 <jmercally@gmail.com>
2025-07-24 11:27:01 -0600

    new project

---
+++ proyectosc#/numberPar/Program.cs
@@ -0,0 +1,24 @@
+﻿
+    public class Program
+    {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Comprueba si tu numero es par o impar");
+
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"El numero {number} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {number} es impar");
+        }
+
+        Console.WriteLine("<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>");
+        string resultado = (number % 2 == 0) ? "numero par" : "numero impar";
+
+        Console.WriteLine($"El numero {number} es {resultado}");
+        }
+    }
