d64f18c6183b8a0b876cc7a07a568b3274ba506d
mercally1 <jmercally@gmail.com>
2025-08-07 08:29:17 -0600

    new proyect and polindromo

---
+++ proyectosc#/polingromo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string bienvenida = "las palabras polindromos";
+        Console.WriteLine(bienvenida.ToUpper());
+        string palabra = Console.ReadLine();
+        string palabra1 = new string(palabra.Reverse().ToArray());
+
+        string polindromo = palabra == palabra1 ? $" Palabra original {palabra.ToUpper()}\n Palabra invertida {palabra1.ToUpper()} es una palabra polidromo" 
+                                                : $" Palabra original {palabra.ToUpper()}\n Palabra invertida {palabra1.ToUpper()} no es una palabra polindromo";
+
+        Console.WriteLine(polindromo); 
+        
+    }
+}
