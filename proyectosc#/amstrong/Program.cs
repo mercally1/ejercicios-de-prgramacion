655fd8baac4fd0b8d5e30c7dddaaa145b0ade9d2
mercally1 <jmercally@gmail.com>
2025-08-07 11:22:52 -0600

    numeros amstrong

---
+++ proyectosc#/amstrong/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Introduce un numero");
+        int numero = int.Parse(Console.ReadLine());
+        int suma = 0;
+        int temp = numero;
+        int digitos = numero.ToString().Length;
+
+
+        while (temp > 0)
+        {
+            int digito = temp % 10;
+            suma += (int)Math.Pow(digito, digitos);
+            temp /= 10;
+        }
+
+        if (suma == numero)
+        {
+            Console.WriteLine($"el numero {numero} es un numero amstrong");
+        }
+        else
+        {
+            Console.WriteLine($"el numero {numero} NO es un numero amstrong");
+        }
+    }
+}
